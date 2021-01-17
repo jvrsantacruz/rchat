59a79a0c3e739b292ccaf9c1892f17bb093ba9b4
Javier Santacruz <javier.santacruz.lc@gmail.com>
2021-01-17 21:59:17 +0100

    Adds pypi package building

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ import re
 from setuptools import setup, find_packages
 
 
+def get_version_from_debian_changelog():
+    try:
+        with io.open('debian/changelog', encoding='utf8') as stream:
+            return re.search(r'\((.+)\)', next(stream)).group(1)
+    except Exception:
+        return '0.0.1'
+
+
 setup(
     name='rchat',
     description='A simple RocketChat command line client',
     long_description=io.open('README.md').read(),
     long_description_content_type='text/markdown',
-    version='0.0.1',
+    version=get_version_from_debian_changelog(),
     include_package_data=True,
     author='Javier Santacruz',
     author_email='javier.santacruz.lc@gmail.com',
@@ -26,6 +34,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ),
 )
