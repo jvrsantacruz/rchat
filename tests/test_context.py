7ad00c08860ca0eb8e9939627594ae709b2671b9
Javier Santacruz <javier.santacruz@avature.net>
2021-01-19 23:15:39 +0100

    Adds support for config backwards compatibility

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,6 +1,5 @@
 import pytest
-from hamcrest import assert_that, has_entries
-
+from hamcrest import assert_that, has_entries, instance_of, is_
 from rchat.cli import Context
 
 URL = "https://localhost:9999"
@@ -31,6 +30,13 @@ class TestContext:
         )
 
     def test_it_should_take_user_credentials(self):
-        ctx = Context(url=URL, user=USER, password=PASS)
+        ctx = Context.build(dict(url=URL, user=USER, password=PASS))
 
         assert_that(ctx.credentials, has_entries(user=USER, password=PASS))
+
+    def test_it_should_not_fail_with_invalid_fields(self):
+        ctx = Context.build(
+            dict(url=URL, user=USER, password=PASS, unkown="💩")
+        )
+
+        assert_that(ctx, is_(instance_of(Context)))
